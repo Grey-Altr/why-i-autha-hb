ca7f1dc6f4d74661b36548fe696bd07e65f48d90
Grey Altr <grey.sifr@gmail.com>
2022-11-16 14:08:01 -0800

    pause signIn build, to build signUp

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+import { redirectIfLoggedIn, signInUser } from './fetch-utils.js';
+
 const signInForm = document.getElementById('sign-in');
+const signInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
 
 const signUpForm = document.getElementById('sign-up');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('');
 
 // Wire up sign in and sign up forms to supabase
-// Redirect to /other-page on successful auth
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const user = await signInUser(signInEmail.value, signInPassword.value);
+    // Redirect to /other-page on successful auth
+    if (user) {
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
 // Redirect to /other-page when page loads if user is authenticated
